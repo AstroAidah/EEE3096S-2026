e3650588e189a4cd879dea328b4b451b851b62f5
AstroAidah <166826358+AstroAidah@users.noreply.github.com>
2026-08-21 14:58:49 +0200

    Implement ADC to DAC loop with delay in dsp.s

--- Practical_1B/Practical1B-Assembly/Core/Src/dsp.s
+++ Practical_1B/Practical1B-Assembly/Core/Src/dsp.s
@@ -3,8 +3,8 @@
  * EEE3096S 2026 - Practical 1B, Task 4
  * Cycle-counted ADC to DAC loop with a 45 degree phase delay
  *
- * Student 1 : <name>  <student number>
- * Student 2 : <name>  <student number>
+ * Student 1 : Mati Taimu  TMXMAT005
+ * Student 2 : Idah Sumbi  SMBIDA001
  */
 
     .syntax unified
@@ -34,22 +34,29 @@ DSP_Loop:
 loop:
     @ --- SAMPLE AND OUTPUT ------------------------------------------------
     @ TODO 1: Read the current ADC conversion from the Data Register.
+    LDR R2, [R0] @places whats being pointed at with R0 and puts it in R2
     
     @ TODO 2: Write the value straight out to the DAC Data Register.
+    STR R2, [R1] @take whats in R2 and put it where R1 is pointing
 
     @ --- DELAY SETUP ------------------------------------------------------
     @ TODO 3: Calculate the required cycle target for a 45-degree phase 
     @         delay on a 1 kHz sine wave running at an 8 MHz system clock.
     @         Load your inner loop counter and insert any NOP padding 
     @         needed to hit your exact target.
+    MOVS R3, #248  @countdown from 248
+    NOP
 
 delay_loop:
     @ --- INNER LOOP -------------------------------------------------------
     @ TODO 4: Implement the counted delay loop.
     @         (Remember to use flag-updating arithmetic so your branch works).
+    SUBS R3, R3, #1 @R3=R3-1
+    BNE  delay_loop
 
     @ --- REPEAT -----------------------------------------------------------
     @ TODO 5: Branch back to the start of the main 'loop'.
+    B loop
     
     @ ----------------------------------------------------------------------
     @ NOTE: You must calculate your exact cycle budget, showing the cost 
